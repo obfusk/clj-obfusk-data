31eee4b73c232e2f1bd4e4e7ebf23262ee673e87
Felix C. Stegerman <flx@obfusk.net>
2013-01-12 17:55:48 +0100

    fix speclj/marginalia config for lein2

--- project.clj
+++ project.clj
@@ -2,9 +2,10 @@
   :description  "..."
   :url          "https://bitbucket.org/obfusk/loki-util"
 
-  :dependencies     [ [org.clojure/clojure "1.4.0"] ]
+  :dependencies [ [org.clojure/clojure  "1.4.0"]
+                  [speclj               "2.5.0"] ]
 
-  :dev-dependencies [ [speclj           "2.1.1"]
-                      [lein-marginalia  "0.7.1"] ]
+  :plugins      [ [speclj               "2.5.0"]
+                  [lein-marginalia      "0.7.1"] ]
 
-  :test-path "spec/" )
+  :test-paths   ["spec/"] )
