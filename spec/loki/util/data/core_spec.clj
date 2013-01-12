d79fb9d295246a9d3b619a979a0a09553610fa8f
Felix C. Stegerman <flx@obfusk.net>
2013-01-12 18:29:01 +0100

    ...

---
+++ spec/loki/util/data/core_spec.clj
@@ -0,0 +1,28 @@
+; --                                                              {{{1
+
+;;      File        : loki/util/data/core_spec.clj
+;;      Maintainer  : Felix C. Stegerman <flx@obfusk.net>
+;;      Date        : 2013-01-12
+;;
+;;      Copyright   : Copyright (C) 2013  Felix C. Stegerman
+;;      Licence     : GPLv2 or EPLv1
+
+; --                                                              }}}1
+
+;; ...
+
+(ns loki.util.data.core-spec
+  (:use speclj.core)
+  (:require [loki.util.data.core :as _d]) )
+
+(describe "loki.util.data.core"
+
+  (it "..."
+    true)
+
+  ; ...
+)
+
+(run-specs)
+
+; vim: set tw=70 sw=2 sts=2 et fdm=marker :
---
+++ src/loki/util/data/core.clj
@@ -0,0 +1,18 @@
+; --                                                              {{{1
+
+;;      File        : loki/util/data/core.clj
+;;      Maintainer  : Felix C. Stegerman <flx@obfusk.net>
+;;      Date        : 2013-01-12
+;;
+;;      Copyright   : Copyright (C) 2013  Felix C. Stegerman
+;;      Licence     : GPLv2 or EPLv1
+
+; --                                                              }}}1
+
+;; ...
+
+(ns loki.util.data.core)
+
+; ...
+
+; vim: set tw=70 sw=2 sts=2 et fdm=marker :
